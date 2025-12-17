59937a404a054c47b7b786fc326f84651091d949
Pete Blackburn <p.d.blackburn0@gmail.com>
2025-12-17 15:19:56 +0000

    Enter funtion now working through Javascript when answer is submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    runGame("addition");
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
 
+    runGame("addition"); // Start the game after DOM is loaded
 });
+
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
