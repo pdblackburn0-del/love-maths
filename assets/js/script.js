57c4f9b5c7ec723341cb6aa168e3a44bc6c5f020
Pete Blackburn <p.d.blackburn0@gmail.com>
2025-12-17 13:55:27 +0000

    Added multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,11 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+
+    }
+     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -69,6 +73,11 @@ function calculateCorrectAnswer() {
             answer: operand1 + operand2,
             gameType: "addition",
         };
+    } else if (operator === "×") {
+        return {
+            answer: (operand1 * operand2),
+            gameType: "multiply",
+        };
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -102,4 +111,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1,operand2) {
+      document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "×";
+}
